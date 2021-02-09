6af91310a7dcf4acab9563d1fc66bbc0407c4789
Yuma-Saito <yuma.s8102@gmail.com>
2021-02-09 23:46:57 +0900

    とりあえず全部フォームにハンドラを仕掛け動作確認

--- js/front.js
+++ js/front.js
@@ -1,1 +1,369 @@
 
+/**
+ * front.js
+ * 
+ * フロントUIに関する処理
+ */
+
+
+/* ============================== ファイル処理系 ============================== */
+
+/**
+ * 新規盤面作成
+ */
+function newFile(evt) {
+  alert('New File');
+  // Sudokizer.astack.clear(action);
+}
+
+/**
+ * ぱずぷれURL出力
+ */
+function urlWritePuzpre(evt) {
+  alert('URL write for puzpre');
+}
+
+/**
+ * puzzlink URL出力
+ */
+function urlWritePuzzlink(evt) {
+  alert('URL write for puzzlink');
+}
+
+/**
+ * 画像出力
+ */
+function imgWrite(evt) {
+  // 1. ファイル名取得
+  // 2. 画像保存
+  alert('Image Write');
+}
+
+/**
+ * pencilBox形式読み込み
+ */
+function pencilBoxRead(evt) {
+  alert('pencilBox Read');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * pencilBox出力
+ */
+function pencilBoxWrite(evt) {
+  alert('pencilBox Write');
+}
+
+/**
+ * 独自テキスト形式読み込み
+ */
+function easyTextRead(evt) {
+  alert('easytext read');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 独自テキスト形式出力
+ */
+function easyTextWrite(evt) {
+  alert('easytext write');
+}
+
+
+/**
+ * URL出力関数メインルーチン
+ */
+function urlWrite(board) {
+  // ?以降の部分を生成
+  prefix = board['puzname'] + '/' + board['cols'] + '/' + board['rows'] + '/';
+  // boardの中身から読み取っていく処理
+  // ヒント数字は1,2,3,4,5,6,7,8,9
+  // ?ヒントは"." 
+  // 空白はg=1, h=2, i=3, ..., z=20 単位
+  // 解答と候補は無視
+  alert('URL Write');
+}
+
+/**
+ * URL入力関数メインルーチン
+ */
+function urlRead(urlstring) {
+  // 1. ?の後ろのみ取り出す
+  // 2. 3つ目の/以降を取り出す
+  // 3. urlWriteと逆の処理でboardを生成
+}
+
+
+
+
+/* =========================== 盤面変換系 ================================= */
+
+/**
+ * 現在の盤面を新しいウィンドウにコピー
+ */
+function boardNewWindow(evt) {
+  alert('Copy Board');
+}
+
+/**
+ * 90度右回転
+ */
+function boardRotate90Deg(evt) {
+  alert('rotate 90deg right');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 180度右回転
+ */
+function boardRotate180Deg(evt) {
+  alert('rotate 180deg');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 90度左回転
+ */
+function boardRotate270Deg(evt) {
+  alert('rotate 90deg left');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 上下反転
+ */
+function boardInverseUD(evt) {
+  alert('inverse up-to-down');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 左右反転
+ */
+function boardInverseLR(evt) {
+  alert('inverse left-to-right');
+  // Sudokizer.astack.push(action);
+}
+
+
+/* ============================= 表示設定系 ================================ */
+
+/**
+ * 表示サイズ設定
+ */
+function setCellSize(evt) {
+  alert('set cell size');
+  redraw();
+}
+
+/**
+ * フォント設定
+ */
+function setFont(evt) {
+  alert('set font');
+  redraw();
+}
+
+/**
+ * 表示色設定
+ */
+function setColor(evt) {
+  alert('set color');
+  redraw();
+}
+
+/**
+ * 色設定デフォルト
+ */
+function setColorDefault(evt) {
+  // defcolorset を colorset に shallow copy
+  Sudokizer.config.colorset = Object.assign({}, Sudokizer.config.defcolorset);
+  alert('reste color settings');
+  redraw();
+}
+
+/* ============================= デバッグモード ============================= */
+
+/**
+ * デバッグモード切替。完成。
+ */
+function debugMode(evt) {
+  Sudokizer.config.debugmode = !Sudokizer.config.debugmode;
+  alert('debug mode');
+}
+
+/* ============================= 入力モード設定 ============================== */
+
+/**
+ * 問題入力モード
+ */
+function setQMode(evt) {
+  Sudokizer.config.qamode = 'question';
+  alert('Qmode')
+  redraw();
+}
+
+/**
+ * 解答入力モード
+ */
+function setAMode(evt) {
+  Sudokizer.config.qamode = 'answer';
+  alert('Amode');
+  redraw();
+}
+
+/**
+ * 候補入力モード
+ */
+function switchKMode(evt) {
+  Sudokizer.config.kouhomode = !Sudokizer.config.kouhomode;
+  alert('Kmode');
+  redraw();
+}
+
+/* ================================ 盤面編集 ================================= */
+
+/**
+ * undo
+ */
+function actionUndo(evt) {
+  // とりあえずここにメモ: ActionStackのメソッド
+  // - undo：ポインタを1個前に戻して盤面をチェックアウト + redraw()
+  // - redo：ポインタを1個次に進めて盤面をチェックアウト + redraw()
+  // - push：現在のポインタの位置にアクションを「破壊的に」追加 + redraw()
+  // - clear: actionstack初期化 + redraw()
+
+  // Sudokizer.astack.undo();
+  alert('undo')
+}
+
+/**
+ * redo
+ */
+function actionRedo(evt) {
+  // Sudokizer.astack.redo();
+  alert('redo');
+}
+
+/**
+ * 解答消去
+ */
+function answerClear(evt) {
+  alert('clear answer');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 候補消去
+ */
+function kouhoClear(evt) {
+  alert('clear kouho');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 仮定レベル変更
+ */
+function setKateiLevel(evt) {
+  alert('katei Level');
+  redraw();
+}
+
+
+
+/* ================================ 制作支援 ================================= */
+
+/**
+ * 解答チェック
+ */
+function answerCheck(evt) {
+  alert('answer check');
+}
+
+/**
+ * 候補自動洗い出し
+ */
+function autoKouho(evt) {
+  alert('auto kouho calculate');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * ？ヒント条件から半自動生成
+ */
+function autoGenerate(evt) {
+  alert('semi-auto generation via ? hints');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 1ステップ解答
+ */
+function stepSolve(evt) {
+  alert('stepsolve');
+  // Sudokizer.astack.push(action);
+}
+
+/**
+ * 全ステップ解答
+ */
+function allSolve(evt) {
+  alert('allSolve');
+  // Sudokizer.astack.push(action);
+}
+
+
+/* ================================ Canvas ================================== */
+
+/**
+ * 盤面へのクリック
+ */
+function clickBoard(evt) {
+  // alert('board clicked');
+  console.log(evt);
+  // Sudokizer.astack.push(action);
+}
+/**
+ * 盤面へのキーボード押下
+ */
+function keyDownBoard(evt) {
+  alert('keyboard downed');
+  console.log(evt);
+  // Sudokizer.astack.push(action);
+}
+
+
+/**
+ * 盤面再描画用呼び出しルーチン
+ * Sudokizerの中身が変わった際に呼び出す実装
+ */
+function redraw() {
+  drawBoard(Sudokizer.board);
+}
+
+/**
+ * 盤面のメイン描画ルーチン
+ */
+function drawBoard(board) {
+  alert('darw Board');
+  // 設定に応じてcanvasかsvgか変更
+  if (Sudokizer.config.drawmedia === 'canvas') {
+    drawBoardCanvas(board);
+  } else {
+    drawBoardSVG(board);
+  }
+}
+
+/**
+ * canvasへの描画
+ */
+function drawBoardCanvas(board) {
+  alert('draw Board Canvas');
+}
+
+/**
+ * SVGへの描画
+ */
+function darwBoardCanvas(board) {
+  alert('draw Board SVG');
+}
--- js/init.js
+++ js/init.js
@@ -5,39 +5,68 @@
  * ページ読み込み時に実行する初期化用スクリプト
  */
 
-// Sudokizerグローバルオブジェクト
+/**
+ * グローバル変数類の初期化処理
+ */
+
+// Sudokizerグローバル変数。
+// グローバル設定類はすべてこのオブジェクトに格納
 let Sudokizer = {};
 
-Sudokizer.board = initBoard();         // 初期盤面の作成
-Sudokizer.config = getFormConfig();    // フォーム設定類
-// 暫定
-console.log(Sudokizer);
+Sudokizer.astack = initActionStack();   // アクションスタック作成
+Sudokizer.board  = initBoard();         // 初期盤面の作成
+Sudokizer.config = initConfig();        // フォーム設定類
 
-// イベントハンドラの設定
-setEventHandlers();
+setEventHandlers();   // イベントハンドラを仕掛ける
+redraw();             // 初回描画
 
 
-// 暫定実装
+
+/**
+ * クラス定義
+ */
+
+
+/**
+ * ページロード時の初期盤面作成
+ */
 function initBoard() {
   // メモ
   // 1. Boardクラスのコンストラクタを呼び出して空盤面生成
   // 2. URLのGETパラメータがあればそこから盤面を構築する関数を呼び出す
   return {'Board': 'OK'};
 }
-function getFormConfig() {
-  // 含める項目メモ
-  // - 表示サイズ
-  // - フォント
-  // - 各種表示色（背景、ヒント、入力、除外候補、第1～第4仮定）
-  // - 問題/解答モード
-  // - 候補入力モード
-  // - 仮定レベル
+
+/**
+ * グローバル設定オブジェクトの作成
+ */
+function initConfig() {
+
+  let colorset = {'bg': '#ffffff'}   // 色設定をHTMLから取得
+  const defcolorset = Object.assign({}, colorset);
+
   return {
     'dispsize': 32,
-    'dispfont': 'ゴシック体'
+    'dispfont': 'ゴシック体',
+    'colorset': colorset,
+    'defcolorset': defcolorset,
+    'qamode': 'question',
+    'kouhomode': false,
+    'kateilevel': 0,
+    'drawmedia': 'canvas',
+    'debugmode': false,
   };
 }
 
+/**
+ * アクションスタック作成。
+ * これはコンストラクタ呼び出しでいいかもしれない。
+ */
+function initActionStack() {
+  return {
+    'ActionStack': 'OK',
+  };
+}
 
 /**
  * 各種ボタン、フォームにイベントハンドラを仕掛ける
@@ -45,83 +74,83 @@ function getFormConfig() {
 function setEventHandlers() {
   // -------- メインメニュー：ファイル --------
   // 新規作成
-  $('#menu_newfile').click(alert_test);
+  $('#menu_newfile').click(newFile);
   // ぱずぷれURL出力
-  $('#menu_urlwrite_puzpre').click(alert_test);
+  $('#menu_urlwrite_puzpre').click(urlWritePuzpre);
   // puzzlink URL出力
-  $('#menu_urlwrite_puzzlink').click(alert_test);
+  $('#menu_urlwrite_puzzlink').click(urlWritePuzzlink);
   // 画像出力
-  $('#menu_imgwrite_ok').click(alert_test);
+  $('#menu_imgwrite_ok').click(imgWrite);
   // pencilbox読み込み
-  $('#menu_pbread_ok').click(alert_test);
+  $('#menu_pbread_ok').click(pencilBoxRead);
   // pencilbox出力
-  $('#menu_pbwrite_ok').click(alert_test);
+  $('#menu_pbwrite_ok').click(pencilBoxWrite);
   // テキスト読み込み
-  $('#menu_txtread_ok').click(alert_test);
+  $('#menu_txtread_ok').click(easyTextRead);
   // テキスト書き込み
-  $('#menu_txtwrite_ok').click(alert_test);
+  $('#menu_txtwrite_ok').click(easyTextWrite);
   
   // -------- メインメニュー：編集 ----------
   // 盤面の複製
-  $('#menu_boardcopy').click(alert_test);
+  $('#menu_boardcopy').click(boardNewWindow);
   // 盤面90回転
-  $('#menu_rotate90').click(alert_test);
+  $('#menu_rotate90').click(boardRotate90Deg);
   // 盤面180回転
-  $('#menu_rotate180').click(alert_test);
+  $('#menu_rotate180').click(boardRotate180Deg);
   // 盤面270回転
-  $('#menu_rotate270').click(alert_test);
+  $('#menu_rotate270').click(boardRotate270Deg);
   // 盤面上下反転
-  $('#menu_inverse_ud').click(alert_test);
+  $('#menu_inverse_ud').click(boardInverseUD);
   // 盤面左右反転
-  $('#menu_inverse_lr').click(alert_test);
+  $('#menu_inverse_lr').click(boardInverseLR);
 
   // -------- メインメニュー：表示 --------
   // 表示サイズ
-  $('#menu_dispsize_ok').click(alert_test);
+  $('#menu_dispsize_ok').click(setCellSize);
   // フォント
-  $('[id^="menu_font"]').click(alert_test);
+  $('[id^="menu_font"]').click(setFont);
   // 色設定
-  $('[id^="menu_dispcolor"]').change(alert_test);
+  $('[id^="menu_dispcolor"]').change(setColor);
   // 色設定デフォルト
-  $('#menu_color_reset').click(alert_test);
+  $('#menu_color_reset').click(setColorDefault);
   // デバッグモード
-  $('#menu_debugmode').click(alert_test);
+  $('#menu_debugmode').click(debugMode);
 
   // -------- 右フォーム：入力モード --------
   // 問題入力モード
-  $('#opform_qmode').click(alert_test);
+  $('#opform_qmode').click(setQMode);
   // 解答入力モード
-  $('#opform_amode').click(alert_test);
+  $('#opform_amode').click(setAMode);
   // 候補入力モード
-  $('#opform_kmode').click(alert_test);
+  $('#opform_kmode').click(switchKMode);
   
   // -------- 右フォーム：編集 --------
   // アンドゥ
-  $('#opform_undo').click(alert_test);
+  $('#opform_undo').click(actionUndo);
   // リドゥ
-  $('#opform_redo').click(alert_test);
+  $('#opform_redo').click(actionRedo);
   // 解答消去
-  $('#opform_ansclear').click(alert_test);
+  $('#opform_ansclear').click(answerClear);
   // 候補消去
-  $('#opform_kouhoclear').click(alert_test);
+  $('#opform_kouhoclear').click(kouhoClear);
   // 仮定レベル
-  $('#opform_kateilevel').change(alert_test);
+  $('#opform_kateilevel').change(setKateiLevel);
 
   // -------- 右フォーム：制作支援 --------
   // 解答チェック
-  $('#opform_anscheck').click(alert_test);
+  $('#opform_anscheck').click(answerCheck);
   // 自動候補入力
-  $('#opform_autokouho').click(alert_test);
+  $('#opform_autokouho').click(autoKouho);
   // 半自動生成
-  $('#opform_autogen').click(alert_test);
+  $('#opform_autogen').click(autoGenerate);
   // 1ステップ解答
-  $('#opform_stepsolve').click(alert_test);
+  $('#opform_stepsolve').click(stepSolve);
   // 全回答
-  $('#opform_autosolve').click(alert_test);
+  $('#opform_autosolve').click(allSolve);
 
+  // -------------- Canvas --------------
+  $('#main_board').click(clickBoard);
+  $('#main_board').keydown(keyDownBoard);
 }
 
-// 仮
-function alert_test(e) {
-  alert(e);
-}
+
